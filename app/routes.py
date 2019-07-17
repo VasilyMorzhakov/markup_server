0c7d738d5b8c75ae35d42152d6649c834616ec54
VasilyMorzhakov <v.morzhakov@cherryhome.ai>
2019-07-17 14:47:46 +0300

    applications in config

--- app/routes.py
+++ app/routes.py
@@ -7,6 +7,19 @@ import os
 import shutil
 import random
 
+temp={}
+temp['applications']=[]
+temp['applications'].append('bus')
+temp['applications'].append('one_rect')
+temp['applications'].append('three_rect')
+temp['applications'].append('one_point')
+
+
+f=open(os.path.join(os.path.dirname(os.path.realpath(__file__)),'config.json'),'r+')
+config=json.load(f)
+f.close()
+
+
 width=600
 height=600
 
@@ -15,40 +28,52 @@ def index():
     return "This is a markup service"
 
 
-@app.route('/markup')
-def markup():
-    return render_template('markup.html',width=width,height=height)
-
-@app.route('/images/<path:filename>')
-def return_image(filename):
-    return send_from_directory('images',filename)
-
-@app.route('/get_random_pic_name')
-def get_random_pic_name():
-    dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'images')
-    files=os.listdir(dir)
-    if len(files)==0:
-        return "images/fail"
+@app.route('/markup/<string:application>')
+def markup(application):
+    if application in config['applications']:
+        return render_template(application+'.html',width=width,height=height)
     else:
-        index=random.randint(0,len(files)-1)
-        return 'images/'+files[index]
+        return "no such application"
+
+@app.route('/<string:application>/images/<path:filename>')
+def return_image(application,filename):
+
+    return send_from_directory(os.path.join(os.path.dirname(os.path.realpath(__file__)),'data',application,'images'),filename)
+
+@app.route('/<string:application>/get_random_pic_name')
+def get_random_pic_name(application):
+
+    dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),'data',application,'images')
+    if os.path.exists(dir):
+        files=os.listdir(dir)
+        if len(files)==0:
+            return "/"+application+"/images/fail"
+        else:
+            index=random.randint(0,len(files)-1)
+            return "/"+application+'/images/'+files[index]
+    else:
+        return "None"
 
 
-@app.route('/save',methods=['POST'])
-def savePost():
-    data = request.data
+@app.route('/save/<string:application>',methods=['POST'])
+def savePost(application):
 
-    dict=json.loads(data.decode())
-    fn=dict['imageName']
-    fn_json=fn.replace('.jpg','.json')
-    dir=os.path.dirname(os.path.realpath(__file__))
+    if application in config['applications']:
+        data = request.data
 
-    f=open(os.path.join(dir,fn_json),'wb')
-    f.write(data)
-    f.close()
+        dict=json.loads(data.decode())
+        fn=dict['imageName']
+        fn_json=fn.replace('.jpg','.json')
+        dir=os.path.dirname(os.path.realpath(__file__))
 
-    #move to processed
-    shutil.move(os.path.join(dir,fn),os.path.join(dir,fn.replace('images/','processed/')))
-    shutil.move(os.path.join(dir, fn_json), os.path.join(dir, fn_json.replace('images/', 'processed/')))
+        f=open(dir+'/data'+fn_json,'wb')
+        f.write(data)
+        f.close()
 
-    return "ok"
+        #move to processed
+        shutil.move(dir+'/data'+fn,dir+'/data'+fn.replace('images/','processed/'))
+        shutil.move(dir+'/data'+fn_json, dir+'/data'+fn_json.replace('images/','processed/'))
+
+        return "ok"
+    else:
+        return "no such application"
